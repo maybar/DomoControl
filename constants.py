243d8d90e5c5e6efe436c6234041da8559550459
Miguel Aybar <maaybars@gmail.com>
2018-11-11 15:06:24 +0100

    Primera entraga de archivo de constantes

---
+++ constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# Constants definition
+TX_RESEND = 60 #Time in seconds to resend the radio command to 'Caldera'
+
+PIN_SENSOR_TEMP = 12    # INPUT Pin to read the temperature
+PIN_PIR = 20            # INPUT Pin for movement detection (PIR)
+PIN_LED_A = 6
+PIN_LED_B = 5
+PIN_LDR_CHARGE = 19
+PIN_LDR_DISCHARGE = 13
+PIN_TX = 14             # OUTPUT Pin to radio communication

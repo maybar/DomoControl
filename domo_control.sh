b931b971bf1268e927356a86aa4376dffb11961f
Miguel Aybar <maaybars@gmail.com>
2019-01-04 11:06:27 +0100

    -New operation for the green led

--- domo_control.sh
+++ domo_control.sh
@@ -8,6 +8,11 @@ python3 -m http.server --cgi &
 
 sleep 1
 sudo pigpiod
+pigs m 5 w
+pigs m 6 w
+pigs w 6 1
+pigs w 5 0
+
 cd /home/pi/Documents/PhytonFiles/DomoControl/
 /usr/bin/python3 /home/pi/Documents/PhytonFiles/DomoControl/supervisor.py &
 

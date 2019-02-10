86140ab3185a4a309a913bc657d6e5eef27e21f8
Miguel Aybar <maaybars@gmail.com>
2019-02-10 15:07:40 +0100

    - Archivo para permitir solo una instancia. - Lee el archivo de debug

--- supervisor.py
+++ supervisor.py
@@ -3,7 +3,36 @@ import time
 import tools
 import private_data as private
 import datetime
+import pickle
+import fcntl
+import os, sys
 
+
+def lockFile(fp):
+    try:
+        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        return False
+
+    return True
+
+def unlockFile(x):
+    try:
+        fcntl.flock(x, fcntl.LOCK_UN)
+    except IOError:
+        pass
+
+try:
+    fp = open("/var/lock/supervisor.lock", 'w')
+except:
+    print ("Supervisor.py is already running. Exit now!")
+    sys.exit(0)
+    
+if not lockFile(fp):
+    print ("Supervisor.py is already running. Exit now!")
+    sys.exit(0)
+        
+        
 print("******** SUPERVISOR.PY ************")
 state = "IDLE"
 print ("Supervisor>> IDLE")
@@ -12,7 +41,10 @@ process = psutil.Process()
 watchdog_counter = 0
 error_counter = 0
 correo = tools.Email(private)
-str_detail=""
+restart_counter = 0
+current_pid = psutil.Process().pid
+header = "***Supervisor. PID: "+ str(current_pid) +"\nStart time: "+str(datetime.datetime.now()) + "\n"
+str_detail=header
 while(1):
     if state == "IDLE":
         #check if the app is running the first time
@@ -27,13 +59,14 @@ while(1):
                 for word in CL:
                     if '/DomoControl/main.py' in word:
                         if 'sleeping' in ST:
-                            print ("Supervisor>>RUNNING")
+                            print ("IDLE>>RUNNING")
                             state = "RUNNING"
                             found = True
+                            str_detail += "Time: "+str(datetime.datetime.now()) +" IDLE>>RUNNING "+ " Status: "+ ST+ " PID: "+ str(pid)+"\n"
                             break
                     
             except (psutil.ZombieProcess, psutil.AccessDenied, psutil.NoSuchProcess):
-                print ("except")
+                print ("IDLE except checking main process")
             if found == True:
                 break
         
@@ -43,62 +76,70 @@ while(1):
         try:
             ST = process.status()
             if (not psutil.pid_exists(pid)) or (ST == psutil.STATUS_STOPPED) or (ST == psutil.STATUS_ZOMBIE):
-                str_detail = "Status: "+ ST +"\n"
-                str_detail += "PID does not exist\n"
+                str_detail += "Time: "+str(datetime.datetime.now()) +" RUNNING>>STOPPED "+ "Status: "+ ST+ " PID: "+ str(pid)+ " Reason: (PID does not exist)\n"
                 state = "STOPPED"
-                    
+                print ("RUNNING>>STOPPED: Reason: (PID does not exist)")
+            
         except (psutil.ZombieProcess):
-            str_detail += "Process Zombie\n"
+            str_detail += "Time: "+str(datetime.datetime.now()) +" RUNNING>>STOPPED "+ "Status: "+ ST+ " PID: "+ str(pid)+ " Reason: (Process Zombie)\n"
             state = "STOPPED"
+            print ("RUNNING>>STOPPED: Reason: (Process Zombie)")
         except (psutil.NoSuchProcess):
-            str_detail += "No process\n"
+            str_detail += "Time: "+str(datetime.datetime.now()) +" RUNNING>>STOPPED "+ "Status: "+ ST+ " PID: "+ str(pid)+ " Reason:(No process)\n"
             state = "STOPPED"
+            print ("RUNNING>>STOPPED: Reason:(No process)")
         # Check watchdog couter
         try:
-            with open ('config.pickle','rb') as f:
-                config_list = pickle.load(f)
-                f.close()
-                if (watchdog_counter == config_list[1]):
-                    error_counter +=1
-                else:
-                    error_counter = 0
-                if error_counter == 5:
-                    state = "STOPPED"
-                    str_detail = "Status: "+ ST +"\n"
-                    str_detail +='watchdog is blocked\n'
-                watchdog_counter = config_list[1]
+            f = os.open("/var/tmp/debug.txt", os.O_RDONLY | os.O_NONBLOCK)
+            ret = os.read(f,50)
+            os.close(f)
+            if watchdog_counter == ret:
+                error_counter +=1
+            else:
+                error_counter = 0
+            if error_counter == 10:
+                state = "STOPPED"
+                str_detail += "Time: "+str(datetime.datetime.now()) +" RUNNING>>STOPPED "+ "Status: "+ ST+ " PID: "+ str(pid)+ " Reason:(watchdog is blocked)\n"
+                str_detail += "Debug file: "+ret.decode()+"\n"
+                print ("RUNNING>>STOPPED: Reason:(watchdog is blocked)")
+            watchdog_counter = ret
         except Exception as e:
-            pass
+            error_counter +=1
+            print ("Supervisor: Error Opening debug.txt. Error: "+str(e))
         
     elif state == "STOPPED":
-        
-        print(str_detail)
-        try:
-            process.terminate()
-            time.sleep(3)
-            process.kill()
-        except:
-            print ("except terminating process")
-            str_detail += "Error stoping process\n"
+        if error_counter == 10:
+            try:
+                process.terminate()
+                time.sleep(3)
+                process.kill()
+            except:
+                print ("except terminating process")
+                str_detail += "Time: "+str(datetime.datetime.now()) +" STOPPED "+ "Status: "+ ST+ " PID: "+ str(pid)+ " Reason:(Error stoping process)\n"
+                
             
         #try to start again
         process = psutil.Popen("/home/pi/Documents/PhytonFiles/DomoControl/main.sh")
+        restart_counter +=1
         if process is not None:
             print("main.sh restarted ! pid={}".format(process.pid))
-            str_detail += "main.sh restarted ! pid={}\n".format(process.pid)
+            str_detail += "Time: "+str(datetime.datetime.now()) +" STOPPED "+ "main.sh restarted ! pid={}\n".format(process.pid)
+            correo.send_email("Supervisor report","Detail: \n"+str_detail)
             time.sleep(5)
             pid = process.pid
             state = "IDLE"
-            print ("Supervisor>>RUNNING")
-            correo.send_email("Supervisor report","Application restarted \n Time: "+str(datetime.datetime.now())+"\nDetail: \n"+str_detail)
-            str_detail = ""
+            error_counter = 0
+            print ("STOPPED>>IDLE")            
+            str_detail=header
+            str_detail += "Time: "+str(datetime.datetime.now()) +" STOPPED>>IDLE "+ " restart_counter: "+ str(restart_counter)+ "\n"
         else:
             print("main.sh process open fail ! ")
+            str_detail += "Time: "+str(datetime.datetime.now()) +" STOPPED "+ " Reason:(main.sh process open fail)\n"
 
     time.sleep(1)
 
-                
-                
+unlockFile(fp)
+
         
             
 
